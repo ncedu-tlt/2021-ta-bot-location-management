67a9145f955dcc9e71a54668cc4a32810f925456
Essed <41740904+Essed@users.noreply.github.com>
2022-04-04 22:43:16 +0400

    Delete location directory

--- location/src/main/java/com/example/location/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.location.repository;
-
-import com.example.location.model.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, String> {
-
-    Category findCategoryByName(String name);
-}
